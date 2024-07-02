4d5170900e15ddcee77ad55076f66cded618b972
frankbottest <franksmolov@yandex.ru>
2024-07-02 23:52:33 +0600

    Add Exchange pages, article navigation, and bot setup

--- bot.py
+++ bot.py
@@ -1,16 +1,34 @@
 import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, BotCommand
 
-bot = telebot.TeleBot('7221253176:AAFKd3cD0p8Y8DymdLp6eJPTgiGqoCe21DQ')
+API_TOKEN = '7221253176:AAFKd3cD0p8Y8DymdLp6eJPTgiGqoCe21DQ'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+# Установите команды для меню бота
+bot.set_my_commands([
+    BotCommand("start", "Запустить приложение"),
+    BotCommand("restart", "Перезапустить приложение")
+])
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = ReplyKeyboardMarkup(row_width=1)
-    web_app_button = KeyboardButton(
-        text="Open CryptoDex Academy",
+    markup = InlineKeyboardMarkup()
+    web_app_button = InlineKeyboardButton(
+        text="Запустить",
         web_app=WebAppInfo(url="https://frankbottest.github.io/botreact")
     )
     markup.add(web_app_button)
-    bot.send_message(message.chat.id, "Welcome to CryptoDex Academy!", reply_markup=markup)
+    bot.send_message(message.chat.id, "Добро пожаловать в CryptoDex Academy", reply_markup=markup)
+
+@bot.message_handler(commands=['restart'])
+def restart(message):
+    markup = InlineKeyboardMarkup()
+    web_app_button = InlineKeyboardButton(
+        text="Запустить",
+        web_app=WebAppInfo(url="https://frankbottest.github.io/botreact")
+    )
+    markup.add(web_app_button)
+    bot.send_message(message.chat.id, "Приложение перезапущено. Нажмите 'Запустить' для повторного запуска.", reply_markup=markup)
 
 bot.polling()
