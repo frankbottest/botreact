06ee640dca7f3894b66b6ef521d03ae26296d040
frankbottest <franksmolov@yandex.ru>
2024-07-02 22:35:08 +0600

    Initial commit

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+
+bot = telebot.TeleBot('7221253176:AAFKd3cD0p8Y8DymdLp6eJPTgiGqoCe21DQ')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = ReplyKeyboardMarkup(row_width=1)
+    web_app_button = KeyboardButton(text="Open CryptoDex Academy", web_app=WebAppInfo(url="https://github.com/frankbottest/botreact"))
+    markup.add(web_app_button)
+    bot.send_message(message.chat.id, "Welcome to CryptoDex Academy!", reply_markup=markup)
+
+bot.polling()
