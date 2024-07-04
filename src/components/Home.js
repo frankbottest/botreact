00bf1336499f80e13711128c80688abe61cd1637
frankbottest <franksmolov@yandex.ru>
2024-07-04 23:07:12 +0600

    Update Home page buttons for YouTube and Telegram

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { FaYoutube, FaTelegram } from 'react-icons/fa';
 import './Home.css';
+import youtubeIcon from '../assets/youtube.png';
+import telegramIcon from '../assets/telegram.png';
 
 function Home() {
   return (
     <div className="home">
-      <div className="channel-title">CryptoDex Academy</div>
-      <div className="links">
-        <a href="https://youtube.com" className="link-button">
-          <FaYoutube />
+      <h1 className="home-title">CryptoDex Academy</h1>
+      <div className="button-container">
+        <a href="https://www.youtube.com" className="home-button youtube">
+          <img src={youtubeIcon} alt="YouTube" />
           <span>YouTube</span>
         </a>
-        <a href="https://t.me" className="link-button">
-          <FaTelegram />
+        <a href="https://www.telegram.org" className="home-button telegram">
+          <img src={telegramIcon} alt="Telegram" />
           <span>Telegram</span>
         </a>
       </div>
-      <div className="ad-block">Реклама</div>
+      <div className="ad-block">
+        <p>Реклама</p>
+      </div>
     </div>
   );
 }
