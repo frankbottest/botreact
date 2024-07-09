e956669791ec6a58caf2742f6372b816995695d7
frankbottest <franksmolov@yandex.ru>
2024-07-09 20:11:18 +0600

    Update Exchanges page with course-like buttons and styles

--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -10,22 +10,34 @@ function Exchanges() {
   return (
     <div className="exchanges">
       <h1>Биржи</h1>
-      <div className="exchange-buttons">
-        <Link to="/exchanges/bybit" className="exchange-button">
-          <img src={bybitLogo} alt="Bybit" />
-          <span>Bybit</span>
+      <div className="exchange-list">
+        <Link to="/exchanges/bybit" className="exchange-item">
+          <img src={bybitLogo} alt="Bybit" className="exchange-image" />
+          <div className="exchange-content">
+            <h2>Bybit</h2>
+            <p>Торговая платформа с фокусом на деривативы.</p>
+          </div>
         </Link>
-        <Link to="/exchanges/bingx" className="exchange-button">
-          <img src={bingxLogo} alt="BingX" />
-          <span>BingX</span>
+        <Link to="/exchanges/bingx" className="exchange-item">
+          <img src={bingxLogo} alt="BingX" className="exchange-image" />
+          <div className="exchange-content">
+            <h2>BingX</h2>
+            <p>Биржа с широким спектром криптовалют.</p>
+          </div>
         </Link>
-        <Link to="/exchanges/okx" className="exchange-button">
-          <img src={okxLogo} alt="OKX" />
-          <span>OKX</span>
+        <Link to="/exchanges/okx" className="exchange-item">
+          <img src={okxLogo} alt="OKX" className="exchange-image" />
+          <div className="exchange-content">
+            <h2>OKX</h2>
+            <p>Популярная криптовалютная биржа с множеством инструментов.</p>
+          </div>
         </Link>
-        <Link to="/exchanges/binance" className="exchange-button">
-          <img src={binanceLogo} alt="Binance" />
-          <span>Binance</span>
+        <Link to="/exchanges/binance" className="exchange-item">
+          <img src={binanceLogo} alt="Binance" className="exchange-image" />
+          <div className="exchange-content">
+            <h2>Binance</h2>
+            <p>Крупнейшая биржа по объему торгов и пользователям.</p>
+          </div>
         </Link>
       </div>
     </div>
